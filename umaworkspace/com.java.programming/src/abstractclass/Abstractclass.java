68b9fd9459273908d1acee553c4f02ddaf4a9380
Umayal-Chockalingam <Uma Viswanathan@LAPTOP-5M6R3NVB>
2021-06-02 14:39:53 +0530

    Initial Commit

---
+++ umaworkspace/com.java.programming/src/abstractclass/Abstractclass.java
@@ -0,0 +1,19 @@
+package abstractclass;
+
+public abstract class Abstractclass {
+
+	
+	static void show() {
+		System.out.println("I am Abstract class");
+	}
+	
+	//Abstract class is partially complete - incomplete
+	//can have main method, but dont use 'new'
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		show();
+		
+	}
+
+}
